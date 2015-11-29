1fba53a0155d3f9c5285b1b98ba283f247afeced
Kai Hendry <hendry@webconverger.com>
2015-11-29 13:34:05 +0800

    Neighbourhood watch blog

--- setup-redirects.sh
+++ setup-redirects.sh
@@ -2,5 +2,5 @@
 touch /tmp/index.html
 while read path
 do
-	aws --profile hsgpower s3 cp --acl public-read /tmp/index.html s3://natalian.org/$path --website-redirect="http://natalian.org$(dirname ${path#archives})/"
+	aws s3 cp --acl public-read /tmp/index.html s3://natalian/$path --website-redirect="http://natalian.org$(dirname ${path#archives})/"
 done < redirects.txt
